2a9b4a8213cbb457bbea034519cda1d584a84b99
Yash Gautam <yash99gautam@gmail.com>
2024-10-09 23:59:31 +0530

    final end

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // import routes
