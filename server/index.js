47207146fc17bc8bc905e670e2d1214ecfc80373
Yash Gautam <yash99gautam@gmail.com>
2024-10-09 23:31:52 +0530

    project complete

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+// to make input as json
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// import routes
+import authRouter from "./routes/auth.route.js";
+import noteRouter from "./routes/note.route.js";
+
+app.use("/api/auth", authRouter);
+app.use("/api/note", noteRouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
+// error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Serer Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
